25335fd20bff8f110ddd6c43faf93b09ad2dedb9
Makkssimka <swoisait@gmail.com>
2022-08-15 08:59:43 +0300

    Поиск и фильтрация по алфавиту

--- src/views/pages/chat/Contacts.vue
+++ src/views/pages/chat/Contacts.vue
@@ -4,6 +4,7 @@ import { computed, onMounted, ref } from 'vue';
 import { useContactsStore } from '@/stores/contacts';
 import CSearch from '@/components/ui/cSearch.vue';
 import CContactItem from '@/components/ui/cContactItem.vue';
+import _ from 'lodash';
 import type { Ref } from 'vue';
 import type { Contact } from '@/interfaces/contacts';
 
@@ -13,6 +14,27 @@ const search: Ref<string> = ref('');
 
 const contacts = computed(() => {
     return contactsStore.getContacts as Array<Contact>;
+});
+
+const contactsFiltered = computed(() => {
+    let list: Array<Contact> = contacts.value;
+
+    if (search.value) {
+        list = contacts.value.filter((item) => {
+            return (
+                item.full_name
+                    .toLocaleLowerCase()
+                    .includes(search.value.toLocaleLowerCase()) ||
+                item.login
+                    .toLocaleLowerCase()
+                    .includes(search.value.toLocaleLowerCase())
+            );
+        });
+    }
+
+    return _.groupBy(list, (item: Contact) =>
+        item.full_name.trim() ? item.full_name.charAt(0) : item.login.charAt(0)
+    );
 });
 
 onMounted(() => {
@@ -37,13 +59,24 @@ onMounted(() => {
         </div>
 
         <perfect-scrollbar class="contacts__result mt-4">
-            <div class="grid gap-4 grid-cols-1" v-if="contacts.length">
-                <c-contact-item
-                    v-for="contact in contacts"
-                    :key="contact.id"
-                    :item="contact"
-                />
-            </div>
+            <template v-if="contacts.length">
+                <div
+                    class="grid gap-4 grid-cols-1"
+                    v-for="(value, key) of contactsFiltered"
+                >
+                    <div class="mt-4">
+                        <span class="contacts__char text-sm rounded">
+                            {{ key }}
+                        </span>
+                    </div>
+
+                    <c-contact-item
+                        v-for="contact in value"
+                        :key="contact.id"
+                        :item="contact"
+                    />
+                </div>
+            </template>
 
             <div v-else class="text-center">У Вас нет кантактов</div>
         </perfect-scrollbar>
@@ -63,5 +96,15 @@ onMounted(() => {
     top: 80px;
     left: 0;
     right: 0;
+}
+
+.contacts__char {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 24px;
+    width: 24px;
+    background-color: variable.$yellow;
+    color: variable.$black;
 }
 </style>
