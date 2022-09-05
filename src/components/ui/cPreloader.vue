c9a52043aa2c11be1c31ebb716ceb6ef6d4f5e9b
Makkssimka <swoisait@gmail.com>
2022-09-05 09:17:04 +0300

    Компонент предзагрузки

---
+++ src/components/ui/cPreloader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+/**
+ * Компонент отображения анимации
+ * загрузки данных
+ */
+
+import preloaderImg from '@/assets/images/preloader.svg';
+</script>
+
+<template>
+    <div class="preloader">
+        <img :src="preloaderImg" />
+    </div>
+</template>
+
+<style lang="scss">
+.preloader {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+        width: 50px;
+        height: 50px;
+    }
+}
+</style>
